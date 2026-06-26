f608d82d90a0ed6a1b092ac7298261b676311484
sodeindetoluwani85-a11y <sodeindetoluwani85@gmail.com>
2026-06-27 00:20:27 +0100

    Update level5.js

--- level5.js
+++ level5.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    // ==========================
+    // PAGE ELEMENTS
+    // ==========================
+
+    const introPage = document.getElementById("introPage");
+    const lettersPage = document.getElementById("lettersPage");
+    const startBtn = document.getElementById("startChallenge");
+
+    // ==========================
+    // START BUTTON
+    // ==========================
+
+    startBtn.addEventListener("click", () => {
+
+        // Hide intro
+        introPage.classList.add("hidden");
+
+        // Show letters page
+        lettersPage.classList.remove("hidden");
+
+    });
+
+});
