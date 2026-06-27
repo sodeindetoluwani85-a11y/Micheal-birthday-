7a1743ad3ebc12357150a49fc569a867ac4d9e08
sodeindetoluwani85-a11y <sodeindetoluwani85@gmail.com>
2026-06-28 00:22:58 +0100

    Update level5.js

--- level5.js
+++ level5.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const introPage = document.getElementById("introPage");
+    const lettersPage = document.getElementById("lettersPage");
+
+    const startBtn = document.getElementById("startBtn");
+
+    // Hide the letters when the page loads
+    lettersPage.style.display = "none";
+
+    startBtn.addEventListener("click", () => {
+
+        introPage.style.display = "none";
+
+        lettersPage.style.display = "flex";
+
+    });
+
+});
